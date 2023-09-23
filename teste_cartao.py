47dea7ca38b59aa9d75f1d50a6a15137112ab880
Daniela <rdaniap.2021@gmail.com>
2023-09-23 17:51:44 -0300

    Teste da classe cartão

--- teste_cartao.py
+++ teste_cartao.py
@@ -1,7 +1,7 @@
 from cartao import Cartao
 
-visa = Cartao("Visa")
-masterCard = Cartao("MasterCard")
+visa = Cartao("1234 5678 9000 0000", "12/30", "123", 50000.00, "Fernando")
+mastercard = Cartao("0000 0012 3456 7899", "12/23", "222", 1000.00, "Daniela")
 
-print(visa.nome)
-print(masterCard.nome)
+print(f"Nome do cliente: {visa.cliente}\n Cartão Nº: {visa.numero}\n limite: {visa.limite}\n status: {visa.status}")
+print(f"Nome do cliente: {mastercard.cliente}\n Cartão Nº: {mastercard.numero}\n limite: {mastercard.limite}\n status: {mastercard.status}")
