2bf9e5c12d52ae4c99f2a084bcd3b59cdf983b71
Daniela <rdaniap.2021@gmail.com>
2023-09-23 17:50:46 -0300

    Inclusao dos atributos no inicializador

--- cartao.py
+++ cartao.py
@@ -1,5 +1,10 @@
 class Cartao:
 
-    def __init__(self, nome):
-        self.nome = nome
-        
+
+    def __init__(self, numero, validade, cvv, limite, cliente):
+        self.numero = numero
+        self.validade = validade
+        self.cvv = cvv
+        self.limite = limite
+        self.cliente = cliente        
+        self.status = "ATIVO"
