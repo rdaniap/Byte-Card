1292ceed55dee959ca613326a79e9e0a28679a6d
Daniela <rdaniap.2021@gmail.com>
2023-09-23 18:27:30 -0300

    Adicionado property nos atributos

--- cartao.py
+++ cartao.py
@@ -14,3 +14,27 @@ class Cartao:
 
     def ativa(self):
         self.__status = "ativo"
+
+    @property
+    def numero(self):
+        return self.__numero
+
+    @property
+    def validade(self):
+        return self.__validade
+
+    @property
+    def cvv(self):
+        return self.__cvv
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @property
+    def cliente(self):
+        return self.__cliente
+
+    @property 
+    def status(self):
+        return self.__status
